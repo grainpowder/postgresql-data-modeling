9b156c702e9d64a584978966b3a2225b89f080d5
grainpowder <siksicking@gmail.com>
2022-09-27 09:10:40 +0900

    fix int type primary key as serial and upsert condition

--- sql_queries.py
+++ sql_queries.py
@@ -8,64 +8,59 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id INT NOT NULL,
-    start_time TIMESTAMP NOT NULL,
-    user_id INT NOT NULL,
-    level VARCHAR,
-    song_id VARCHAR,
-    artist_id VARCHAR,
-    session_id VARCHAR,
-    location VARCHAR,
-    user_agent VARCHAR,
-    PRIMARY KEY (songplay_id)
-);
-""")
-
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id INT NOT NULL,
+    user_id SERIAL PRIMARY KEY,
     first_name VARCHAR,
     last_name VARCHAR,
-    gender VARCHAR,
-    level VARCHAR,
-    PRIMARY KEY (user_id)
+    gender CHAR(1),
+    level VARCHAR
 );
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-    song_id VARCHAR NOT NULL,
+    song_id VARCHAR PRIMARY KEY,
     title VARCHAR NOT NULL,
     artist_id VARCHAR NOT NULL,
-    year INT,
-    duration VARCHAR NOT NULL,
-    PRIMARY KEY (song_id)
+    year INTEGER,
+    duration VARCHAR NOT NULL
 );
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id VARCHAR NOT NULL,
+    artist_id VARCHAR PRIMARY KEY,
     name VARCHAR NOT NULL,
     location VARCHAR,
     latitude DOUBLE PRECISION,
-    longitude DOUBLE PRECISION,
-    PRIMARY KEY (artist_id)
+    longitude DOUBLE PRECISION
 );
 """)
 
 time_table_create = ("""
 CREATE TABLE IF NOT EXISTS time (
-    start_time TIMESTAMP NOT NULL,
+    start_time TIMESTAMP PRIMARY KEY,
     hour VARCHAR,
     day VARCHAR,
     week VARCHAR,
     month VARCHAR,
-    year INT,
-    weekday VARCHAR,
-    PRIMARY KEY (start_time)
+    year INTEGER,
+    weekday VARCHAR
+);
+""")
+
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays(
+    songplay_id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP REFERENCES time(start_time),
+    user_id INTEGER REFERENCES users(user_id),
+    level VARCHAR,
+    song_id VARCHAR REFERENCES songs(song_id),
+    artist_id VARCHAR REFERENCES artists(artist_id),
+    session_id VARCHAR,
+    location VARCHAR,
+    user_agent VARCHAR
 );
 """)
 
@@ -83,8 +78,8 @@ user_table_insert = ("""
 INSERT INTO users 
 (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (user_id)
-DO NOTHING
+ON CONFLICT (user_id) 
+DO UPDATE SET level=EXCLUDED.level
 """)
 
 song_table_insert = ("""
@@ -123,5 +118,5 @@ WHERE title = %s
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop, songplay_table_drop]
